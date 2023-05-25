d96f05ad8bb3fc6645b59c7efccd9b3bb9513c39
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2023-05-25 17:38:27 +0545

    implemented minimax algorithm where AI never lose

--- sketch.js
+++ sketch.js
@@ -4,21 +4,18 @@ let board = [
   ["", "", ""],
 ];
 
-let players = ["X", "O"];
 
-let currentPlayer;
-let available = [];
 
+let w;
+let h;
+let ai='X'
+let human='O'
+let currentPlayer=human;
 function setup() {
   createCanvas(400, 400);
   frameRate(30);
-  currentPlayer = floor(random(players.length));
-  for (let j = 0; j < 3; j++) {
-    for (let i = 0; i < 3; i++) {
-      available.push([i, j]);
-    }
-  }
-}
+  bestMove();   
+} 
 
 function equals3(a, b, c) {
   return a == b && b == c && a != "";
@@ -48,33 +45,54 @@ function checkWinner() {
   if (equals3(board[2][0], board[1][1], board[0][2])) {
     winner = board[2][0];
   }
+  let openSpots=0;
+  for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++) {
+      if(board[i][j]==''){
+        openSpots++;
+      }
+    }
+  }
+  
 
-  if (winner == null && available.length == 0) {
+  if (winner == null && openSpots == 0) {
     return "tie";
   } else {
     return winner;
   }
 }
 
-function nextTurn() {
-  let index = floor(random(available.length));
-  let spot = available.splice(index, 1)[0];
-  let i = spot[0];
-  let j = spot[1];
-  board[i][j] = players[currentPlayer];
-  currentPlayer = (currentPlayer + 1) % players.length;
-}
-
-// function mousePressed() {
-//   nextTurn();
+// function nextTurn() {
+//   let index = floor(random(available.length));
+//   let spot = available.splice(index, 1)[0];
+//   let i = spot[0];
+//   let j = spot[1];
+//   board[i][j] = players[currentPlayer];
+//   currentPlayer = (currentPlayer + 1) % players.length;
 // }
+
+function mousePressed() {
+  w=width/3;
+  h=height/3;
+  if(currentPlayer==human){
+    //human make turn
+    let i=floor(mouseX/w);
+    let j=floor(mouseY/h);
+
+    //if valid turn
+    if (board[i][j]==''){
+      board[i][j]=human;
+      currentPlayer=ai;
+      bestMove();
+    }
+  }
+}
 
 function draw() {
   background(255);
-  let w = width / 3;
-  let h = height / 3;
   strokeWeight(4);
-
+  w=width/3;
+  h=height/3;
   line(w, 0, w, height);
   line(w * 2, 0, w * 2, height);
   line(0, h, width, h);
@@ -86,10 +104,10 @@ function draw() {
       let y = h * j + h / 2;
       let spot = board[i][j];
       textSize(32);
-      if (spot == players[1]) {
+      if (spot == human) {
         noFill();
         ellipse(x, y, w / 2);
-      } else if (spot == players[0]) {
+      } else if (spot == ai) {
         let xr = w / 4;
         line(x - xr, y - xr, x + xr, y + xr);
         line(x + xr, y - xr, x - xr, y + xr);
@@ -107,7 +125,5 @@ function draw() {
     } else {
       resultP.html(`${result} wins!`);
     }
-  } else {
-    nextTurn();
   }
 }
